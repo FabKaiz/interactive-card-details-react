36186451e5c02f7ca0ffbed0b573088c1635ea01
Fabien Chareun <cacrfabfr@gmail.com>
2022-11-16 15:45:29 +0100

    format

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,19 +2,21 @@ import React from 'react'
 import './Card.scss'
 import cardLogo from '../../assets/card-logo.svg'
 
-const Card = ({cardName, cardNumber, cardMonth, cardYear, cardCvc}) => {
+const Card = ({ cardName, cardNumber, cardMonth, cardYear, cardCvc }) => {
   return (
     <div className="cards__container">
       <div className="card__front">
         <img src={cardLogo} alt="card logo" />
-        <p>{cardNumber || "0000 0000 0000 0000"}</p>
+        <p>{cardNumber || '0000 0000 0000 0000'}</p>
         <div className="card__bottom">
           <p>{cardName || 'Jane Appleseed'}</p>
-          <p>{cardMonth || 'MM'}/{cardYear || 'YY'}</p>
+          <p>
+            {cardMonth || 'MM'}/{cardYear || 'YY'}
+          </p>
         </div>
       </div>
       <div className="card__back">
-        <p>{cardCvc|| '000'}</p>
+        <p>{cardCvc || '000'}</p>
       </div>
     </div>
   )
