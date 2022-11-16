b0f33a67779209ccd08b94c1f38070166ef803bf
Fabien Chareun <cacrfabfr@gmail.com>
2022-11-16 15:45:52 +0100

    added validations and media queries

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,7 +1,9 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './Form.scss'
 import formGradientMobile from '../../assets/bg-main-mobile.png'
+import formGradientDesktop from '../../assets/bg-main-desktop.png'
 import Card from '../Card/Card'
+import formSuccesImg from '../../assets/icon-complete.svg'
 
 const Form = () => {
   const [cardName, setCardName] = useState('')
@@ -10,6 +12,10 @@ const Form = () => {
   const [cardMonth, setCardMonth] = useState('')
   const [cardYear, setCardYear] = useState('')
   const [cardCvc, setCardCvc] = useState('')
+  const [formSuccess, setFormSuccess] = useState(false)
+  const [matches, setMatches] = useState(
+    window.matchMedia('(min-width: 900px)').matches
+  )
 
   const cleanTextCharacters = (text) => {
     return text.replace(/\D/g, '')
@@ -30,7 +36,7 @@ const Form = () => {
 
     if (cardDateValue.length > 1) {
       // check if the value is between 1 or 01 and 12
-      if (!/^([1-9]|[0-1][0-2])$/.test(cleanedMonth)) cleanedMonth = ''
+      if (!/^([0][1-9]|[1][0-2])$/.test(cleanedMonth)) cleanedMonth = ''
     }
 
     setCardMonth(cleanedMonth)
@@ -52,17 +58,24 @@ const Form = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault()
-    // 👇️ validation
-    if (cardName && cardNumber && cardMonth && cardYear && cardCvc !== '') {
-      const cardNum = Number(cardNumber)
-      // 👉️ submit form and show thank you message
-    }
+    // 👉️ submit and hide the form and show thank you message
+    setFormSuccess(true)
   }
+
+  useEffect(() => {
+    window
+      .matchMedia('(min-width: 900px)')
+      .addEventListener('change', (e) => setMatches(e.matches))
+  }, [])
 
   return (
     <div className="form__container">
       <div className="form__gradient">
-        <img src={formGradientMobile} alt="color gradient" />
+        {!matches && (
+          <img src={formGradientMobile} alt="color gradient mobile" />
+        )}
+        {matches && <img src={formGradientDesktop} alt="color gradient" />}
+
         <div className="cards">
           <Card
             cardName={cardName}
@@ -74,75 +87,96 @@ const Form = () => {
         </div>
       </div>
 
-      <div className="form__content">
-        <form onSubmit={handleSubmit}>
-          <label>
-            CardHolder Name
-            <input
-              required
-              type="text"
-              value={cardName}
-              name="cardName"
-              placeholder="e.g. Jane Appleseed"
-              onChange={(e) => setCardName(e.target.value)}
-            />
-          </label>
-          <label>
-            Card Number
-            <input
-              required
-              value={cardNumber}
-              name="cardNumber"
-              maxLength={16}
-              onChange={(e) => handleCardNumber(e.target.value)}
-              type="text"
-              placeholder="e.g. 1234 5678 9123 0000"
-            />
-          </label>
-
-          <div className="form__content-bottom">
-            <label className="form__date-label">
-              Exp. Date (MM/YY)
-              <div className="date__input-container">
-                <input
-                  required
-                  type="text"
-                  maxLength={2}
-                  name="month"
-                  value={cardMonth}
-                  onChange={(e) => handleCardMonth(e.target.value)}
-                  placeholder="MM"
-                />
-                <input
-                  required
-                  type="text"
-                  value={cardYear}
-                  onChange={(e) => handleCardYear(e.target.value)}
-                  maxLength={2}
-                  name="year"
-                  placeholder="YY"
-                />
-              </div>
-            </label>
-
+      {formSuccess ? (
+        <div className="form__success">
+          <img src={formSuccesImg} alt="succes logo" />
+          <h2>Thank you!</h2>
+          <p>We've added your card details</p>
+          <button
+            className="form__button"
+            onClick={() => window.location.reload()}
+          >
+            Continue
+          </button>
+        </div>
+      ) : (
+        <div className="form__content">
+          <form onSubmit={handleSubmit}>
             <label>
-              CVC
+              CardHolder Name
               <input
                 required
-                className="cvc__input"
                 type="text"
-                maxLength={3}
-                name="cardCvc"
-                value={cardCvc}
-                onChange={(e) => handleCardCvc(e.target.value)}
-                placeholder="e.g. 123"
+                value={cardName}
+                name="cardName"
+                placeholder="e.g. Jane Appleseed"
+                pattern="[A-Za-z\s]{1,}"
+                onChange={(e) => setCardName(e.target.value)}
               />
             </label>
-          </div>
+            <label>
+              Card Number
+              <input
+                required
+                value={cardNumber}
+                name="cardNumber"
+                maxLength={16}
+                onChange={(e) => handleCardNumber(e.target.value)}
+                type="text"
+                placeholder="e.g. 1234 5678 9123 0000"
+                pattern="[0-9\s]{16}"
+              />
+            </label>
 
-          <button type="submit">Confirm</button>
-        </form>
-      </div>
+            <div className="form__content-bottom">
+              <label className="form__date-label">
+                Exp. Date (MM/YY)
+                <div className="date__input-container">
+                  <input
+                    required
+                    type="text"
+                    maxLength={2}
+                    name="cardMonth"
+                    value={cardMonth}
+                    onChange={(e) => handleCardMonth(e.target.value)}
+                    placeholder="MM"
+                    pattern="[0-9]+"
+                  />
+                  <input
+                    required
+                    type="text"
+                    value={cardYear}
+                    onChange={(e) => handleCardYear(e.target.value)}
+                    maxLength={2}
+                    name="cardYear"
+                    placeholder="YY"
+                    pattern="[0-9]{2}"
+                  />
+                </div>
+              </label>
+
+              <label>
+                CVC
+                <input
+                  required
+                  className="cvc__input"
+                  type="text"
+                  maxLength={3}
+                  name="cardCvc"
+                  value={cardCvc}
+                  onChange={(e) => handleCardCvc(e.target.value)}
+                  placeholder="e.g. 123"
+                  pattern="[0-9]{3}"
+                />
+              </label>
+            </div>
+
+            <button className="form__button" type="submit">
+              Confirm
+            </button>
+          </form>
+        </div>
+      )}
     </div>
   )
 }
