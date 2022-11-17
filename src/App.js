8e345a7f7adc95fd407a9de6b65af94b7e061c78
Fabien Chareun <cacrfabfr@gmail.com>
2022-11-17 11:12:37 +0100

    added disclaimer component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.scss';
+import Disclaimer from './components/Disclaimer/Disclaimer';
 import Form from './components/Form/Form';
 
 function App() {
   return (
     <div className="App">
       <Form />
+      <Disclaimer />
     </div>
   );
 }
